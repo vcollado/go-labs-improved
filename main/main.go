0cf8c6ec92ab4ec50eb111e34b7f822000c38e7d
vcollado <necseron@gmail.com>
2017-08-19 12:27:42 +0200

    added calls to taskHandler

--- main/main.go
+++ main/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-	tasks "github.com/vcollado/go-todo-list/tasks"
-	types "github.com/vcollado/go-todo-list/types"
+	"github.com/vcollado/go-todo-list/tasks"
+	"github.com/vcollado/go-todo-list/types"
 )
 
 func main() {
 
-	var task types.Task
+	var task1, task2, task3, defaultTask types.Task
 
-	task = tasks.NewTransientTask("name 2", "description 2")
+	task1 = tasks.NewTransientTask("this is the first task", "foo")
+	task2 = tasks.NewTransientTask("this is the second task", "baz")
+	task3 = tasks.NewTransientTask("this is the third task", "bas")
+	defaultTask = tasks.NewTransientTask("this is the default task", "fus")
 
-	println(task.IsStored())
+	var defaultTaskGroup = []types.Task{defaultTask}
+	var taskGroup1 = []types.Task{task2, task3}
+
+	var taskHandler types.TaskHandler
+
+	taskHandler = tasks.NewTaskHandler(defaultTaskGroup...)
+	taskHandler.AddTask(task1)
+	taskHandler.AddTasks(taskGroup1)
+
+	for _, task := range taskHandler.Tasks() {
+		println(task.IsStored())
+	}
+
 }
