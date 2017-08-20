37da16374e4c7ea16c0fd78344d0a8531bfef268
vcollado <necseron@gmail.com>
2017-08-20 12:44:51 +0200

    added task creator, task properties getters, type for Transient Task and modified calls from main fix #2

--- main/main.go
+++ main/main.go
@@ -17,13 +17,18 @@ func main() {
 	runtime.GOMAXPROCS(goMaxProcs)
 
 	var task1, task2, task3, task4 types.Task
+	var task5 types.TransientTask
+	var task6 types.Task
 
 	task1 = tasks.NewTransientTask("foo", "this is the first task")
 	task2 = tasks.NewTransientTask("baz", "this is the second task")
 	task3 = tasks.NewTransientTask("bas", "this is the third task")
 	task4 = tasks.NewTransientTask("bar", "this is the fourth task")
+	task5 = tasks.NewTransientTask("fus", "this is the fifth task")
 
-	tasksGroup := []types.Task{task3, task4}
+	task6 = tasks.NewTask(task5.Name(), task5.Description())
+
+	tasksGroup := []types.Task{task3, task4, task6}
 
 	addTaskChannel := make(chan types.Task)
 	addTasksChannel := make(chan []types.Task)
@@ -42,13 +47,13 @@ func main() {
 	addTasksChannel <- tasksGroup
 
 	// wait for the goroutines to finish
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second)
 	wg.Wait()
 
 	println("taskHandler total tasks: ", len(taskHandler.Tasks()))
-	for _, task := range taskHandler.Tasks() {
-		fmt.Printf("%+v\n", task)
-	}
+	// for _, task := range taskHandler.Tasks() {
+	// 	fmt.Printf("%+v\n", task.Name())
+	// }
 
 	fmt.Printf("main finished")
 }
--- tasks/transientTask.go
+++ tasks/transientTask.go
@@ -7,5 +7,10 @@ type TransientTask struct {
 
 // NewTransientTask creates a new transient task with its basic configuration
 func NewTransientTask(name string, description string) *TransientTask {
-	return &TransientTask{Task{Name: name, Description: description}}
+	return &TransientTask{Task{name, description, false}}
+}
+
+// IsStored checks if the task is stored
+func (transientTask TransientTask) IsStored() bool {
+	return transientTask.stored
 }
